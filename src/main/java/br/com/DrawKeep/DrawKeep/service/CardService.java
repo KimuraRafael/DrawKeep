ff20df302ba225c5a0a1bd77abde1cbdbe379200
Rafael Kimura <rafaelkimura57@gmail.com>
2026-07-16 14:57:55 -0300

    feat: Criando Estrutura mvc da entidade Card

---
+++ src/main/java/br/com/DrawKeep/DrawKeep/service/CardService.java
@@ -0,0 +1,21 @@
+package br.com.DrawKeep.DrawKeep.service;
+
+import br.com.DrawKeep.DrawKeep.entity.Card;
+import br.com.DrawKeep.DrawKeep.repository.CardRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CardService {
+
+    private final CardRepository repository;
+
+    public CardService(CardRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Card> findAll(){
+        return repository.findAll();
+    };
+}
